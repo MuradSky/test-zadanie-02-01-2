b7bfdc258e8d40f07612d596f89bda3ac70c0061
MuradSky <56338336+MuradSky@users.noreply.github.com>
2024-02-26 22:08:21 +0300

    add ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	base: '/test-zadanie-02-01-2/'
 })
